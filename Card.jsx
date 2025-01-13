fa712918d3e48a4668f4d3c7d7c45a12c9d198f1
HILLARY MUGASIA <hillarymugasia1111@gmail.com>
2025-01-13 22:47:00 +0300

    Add files via upload
    Creating a card in react js

---
+++ Card.jsx
@@ -0,0 +1,13 @@
+function Card(){
+    return(
+        <div class="container" className="card">
+            <img src="" alt="" />
+            <h2>my website</h2>
+            <p>This is what im currently learning on React Js</p>
+
+        </div>
+    );
+
+
+}
+export default Card
